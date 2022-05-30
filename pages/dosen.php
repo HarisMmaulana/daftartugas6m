b01909994d5065100b5f4f3373151fc51edd9a4f
HarisMmaulana <104288197+HarisMmaulana@users.noreply.github.com>
2022-05-30 17:31:28 +0800

    table dan tombol

--- pages/dosen.php
+++ pages/dosen.php
@@ -1,1 +1,32 @@
 <h2>Dosen</h2>
+<div class="table_responsive">
+<a href="#" class="btn btn-primary mb-3"><span data-feather="plus"></span>Data Baru</a>
+    <table class="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Nama Dosen</th>
+                <th scope="col">Handphone</th>
+                <th scope="col">Email</th>
+                <th scope="col">Opsi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>tes</td>
+                <td>tes</td>
+                <td>tes</td>
+                <td>tes</td>
+                <td>
+                    <a href="#" class="badge bg-warning">
+                        <span data-feather="edit"></span>
+                    </a>
+                    <a href=""# class="badge bg-danger">
+                        <span data-feather="x-circle"></span>
+                    </a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+</div>
